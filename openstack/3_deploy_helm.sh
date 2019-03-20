0533429049444ccdd7bf3767050677cf3492b014
Bharat Kunwar <b.kunwar@gmail.com>
2019-03-20 14:42:29 +0000

    Source bash environment variables before starting user notebook

--- openstack/3_deploy_helm.sh
+++ openstack/3_deploy_helm.sh
@@ -6,7 +6,7 @@ set -e
 helm repo add pangeo https://pangeo-data.github.io/helm-chart/
 helm repo update
 
-helm install pangeo/pangeo --devel \
+helm install pangeo/pangeo \
    --namespace=${NAMESPACE} --name=pangeohub \
    -f secret_config.yaml \
    -f jupyter_config.yaml \
